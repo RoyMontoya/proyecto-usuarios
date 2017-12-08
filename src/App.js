22e0e71f376af47e8b0d0f28bfb142a45d0c18c7
Roy Montoya <roymontoya89@gmail.com>
2017-12-08 12:54:07 -0600

    preliminary configuration ready

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+  Route,
+  BrowserRouter as Router
+} from 'react-router-dom'
+
+import Home from './views/Home'
+import Login  from './views/Login'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div>
+          <Route exact path ="/" Component={Login}/>
+          <Route path = "home" Component={Home}/>
+        </div>
+      </Router>
     );
   }
 }
