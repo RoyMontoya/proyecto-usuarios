ffce9438e32789f4766bd828070b66d56a7ed686
Roy Montoya <roymontoya89@gmail.com>
2017-12-11 12:38:43 -0600

    finished project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
 import './App.css';
+
 import {
   Route,
   BrowserRouter as Router
 } from 'react-router-dom'
 
 import Home from './views/Home'
-import Login  from './views/Login'
+import Login from './views/Login'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route exact path ="/" Component={Login}/>
-          <Route path = "home" Component={Home}/>
-          <Login></Login>
+          <Route exact path="/" component={Login}/>
+          <Route path="/home" component={Home}/>
         </div>
       </Router>
     );
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,13 +1,42 @@
-import React, { Component } from 'react';
+import React from 'react';
+import '../styles/Home.css'
+import Reflux from 'reflux';
+import AppStore from '../stores/AppStore';
+import Actions from '../actions/Actions';
+import Card from '../components/Card';
 
-class Home extends Component{
-   render(){
-     return (
-       <div>
-          <label>Home</label>
-       </div>
-     );
-   }
+class Home extends Reflux.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+    this.store = AppStore;
+    this.storeKeys = ['data'];
+    this.showUsers = this.showUsers.bind(this);
+  }
+
+  showUsers(e) {
+    Actions.getUsers();
+  }
+  render() {
+    let people = this.state.data.map((currentValue, index, array) => {
+      return (<Card
+          key ={index}
+          id={currentValue.id}
+          first_name={currentValue.first_name}
+          last_name={currentValue.last_name}
+          avatar={currentValue.avatar}/>);
+    });
+    return (<div className="Home">
+      <div className="Home-header-box">
+        <h2 className="Home-header-title">Bienvenido</h2>
+        <button className="Home-header-button" onClick={this.showUsers}>Mostrar Usuarios</button>
+      </div>
+      <div className="Home-body">
+        {people}
+      </div>
+    </div>);
+  }
 }
 
 export default Home;
